6ee10e9f72e3e2240de6b1c6471c17423e48ff16
MM <miguelvazsilva@gmail.com>
2019-02-25 15:57:31 +0000

    Initial Commit

---
+++ com/miguelvazsilva/games/TicTacToe/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Classes used to play a TicTacToe game in one of two modes: "2 Players" or "VS Computer"
+ */
+/**
+ * @since 1.0
+ * @author Miguel Vaz Silva
+ * @version 1.0
+ */
+
+package com.miguelvazsilva.games.TicTacToe;
